a3a91c05da19a3e3c9f68020f06f2a3a493c2000
jenniferadamsphoto <jenniferadamsphoto@gmail.com>
2022-03-04 11:09:43 -0500

    Donut button working

---
+++ DonutMaker.js
@@ -0,0 +1,13 @@
+export default class DonutMaker {
+    constructor(donutCount, donutMultiplier, autoClicker){
+    this.donutCount = 0;
+    this.donutMultiplier = 0;
+    this.autoClicker = 0;
+    }
+
+    addToDonutCount() {
+        this.donutCount ++;
+    }
+    
+
+}
--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-const container = (document.querySelector('.container').innerText =
-  'mmmmmmmmm doughnuts.... ahhhhhhh!');
+import DonutMaker from "./DonutMaker.js";
+const donutMaker = new DonutMaker();
+
+donutMaker.addToDonutCount()
+
+let donutCount = donutMaker.donutCount;
+
+const addDonutBtn = document.querySelector(".donutButton");
+const totalDisplay = document.querySelector(".totalDonutCount")
+addDonutBtn.addEventListener("click", () => {
+  donutMaker.addToDonutCount();
+  totalDisplay.innerText = "Donut Count: " + donutMaker.donutCount;
+})
+
